ce22918324073a187ba9d453cfeab76837233b3a
Drew Dolgert <adolgert@uw.edu>
2021-02-06 23:25:42 -0800

    the only failing tests are actual bad tests

--- test/test_direct.jl
+++ test/test_direct.jl
@@ -1,5 +1,17 @@
 using SafeTestsets
 
+module DirectFixture
+using Distributions: ContinuousUnivariateDistribution
+const CTDist = ContinuousUnivariateDistribution
+
+function hazards(callback::Function, distlist::Array{T, 1}, rng) where T <: CTDist
+    for dist_idx in 1:length(distlist)
+        callback(dist_idx, distlist[dist_idx], 0.0, true, rng)
+    end
+end
+
+end
+
 
 @safetestset markov_direct_blah = "MarkovDirect initial" begin
     using Fleck: MarkovDirect, next
@@ -8,7 +20,8 @@ using SafeTestsets
     md = MarkovDirect()
     distributions = fill(Exponential(1.5), 10)
     rng = MersenneTwister(90422342)
-    when, which = next(md, distributions, rng)
+    current = 0.0
+    when, which = next(md, distributions, current, rng)
     @test when > 0
     @test 1 <= which
     @test which <= 10
@@ -19,10 +32,12 @@ end
     using Fleck: MarkovDirect, next
     using Random: MersenneTwister
     using Distributions: Exponential
+    using ..DirectFixture: CTDist
     md = MarkovDirect()
     distributions = CTDist[]
     rng = MersenneTwister(90497979)
-    when, which = next(md, distributions, rng)
+    current = 0.0
+    when, which = next(md, distributions, current, rng)
     @test isinf(when)
     @test which === nothing
 end
@@ -40,8 +55,9 @@ end
     )
     rng = MersenneTwister(90422342)
     hilo = zeros(Int, 2)
+    curtime = 0.0
     for i in 1:10000
-        when, which = next(md, distributions, rng)
+        when, which = next(md, distributions, curtime, rng)
         hilo[(which - 1) ÷ 10 + 1] += 1
     end
     ci = confint(BinomialTest(hilo[1], sum(hilo), 3 / 5))
