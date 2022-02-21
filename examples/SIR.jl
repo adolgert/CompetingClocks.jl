b8b221827c5ec9b067e6d8a607527e4312d5834c
slwu89 <10673535+slwu89@users.noreply.github.com>
2022-02-21 10:21:40 -0800

    fix SIR example

--- examples/SIR.jl
+++ examples/SIR.jl
@@ -1,8 +1,9 @@
 using Random
+using Plots
 include("../src/sample/direct.jl")
 include("../src/vas.jl")
 
-function sir_vas(N, β, γ)
+function sir_vas(β, c, γ)
     take = [
         1 0;
         1 1;
@@ -13,13 +14,14 @@ function sir_vas(N, β, γ)
         2 0;
         0 1;
     ]
-    rates = [(state) -> Exponential(state[1] * state[2] * β/N), (state) -> Exponential(state[2] * γ)]
+    rates = [(state) -> Exponential(β*c*state[2]/sum(state)*state[1]), (state) -> Exponential(state[2] * γ)]
     (take, give, rates)
 end
 
-take, give, rates = sir_vas(1000, 0.05, 0.25)
-
+p = [0.05,10.0,0.25]; # β,c,γ
 u0 = [990,10,0]; # S,I,R
+
+take, give, rates = sir_vas(p...)
 
 rng = MersenneTwister()
 
@@ -31,16 +33,28 @@ fsm = VectorAdditionFSM(vam, DirectCall(Int))
 
 initial_state = vas_initial(vas, u0)
 
-out = Matrix{Int64}(undef, u0[2] + 2*u0[1] + 1, 3)
+out = Matrix{Float64}(undef, u0[2] + 2*u0[1] + 1, 4)
 
 when, next_transition = simstep!(fsm, initial_state, rng)
 
 event_cnt = 0
-out[event_cnt + 1, :] = u0
+tnow = 0.0
+out[event_cnt + 1, 2:4] = u0
+out[event_cnt + 1, 1] = tnow
 
 while next_transition !== nothing
+    tnow += when
+    out[event_cnt + 1, 1] = tnow
+    out[event_cnt + 1, 2:4] = vam.state
     token = vas_delta(vas, next_transition)
     when, next_transition = simstep!(fsm, token, rng)
     event_cnt += 1
-    out[event_cnt + 1, :] = vam.state
 end
+
+plot(
+    out[1:event_cnt, 1],
+    out[1:event_cnt, 2:4],
+    label=["S" "I" "R"],
+    xlabel="Time",
+    ylabel="Number"
+)
