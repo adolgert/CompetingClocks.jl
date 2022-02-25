82a4755b68cda897ff479ed7ae209d997839439d
slwu89 <10673535+slwu89@users.noreply.github.com>
2022-02-25 11:02:04 -0800

    do not need to specify base namespace

--- src/sample/direct.jl
+++ src/sample/direct.jl
@@ -47,14 +47,14 @@ end
 
 function set_clock!(dc::DirectCall{T}, clock::T, distribution::Exponential,
         enabled::Symbol, rng::AbstractRNG) where {T}
-    if Base.:(==)(enabled, :Enabled)
+    if enabled == :Enabled
         hazard = params(distribution)[1]
         if !haskey(dc.key, clock)
             dc.key[clock] = length(push!(dc.propensity, hazard))
         else
             dc.propensity[dc.key[clock]] = hazard
         end
-    elseif Base.:(==)(enabled, :Changed)  # Why is == not found otherwise?
+    elseif enabled == :Changed  # Why is == not found otherwise?
         dc.propensity[dc.key[clock]] = params(distribution)[1]
     else  # else it's disabled.
         dc.propensity[dc.key[clock]] = 0.0
@@ -67,7 +67,7 @@ function next(dc::DirectCall, when::Float64, rng::AbstractRNG)
     if total > eps(Float64)
         chosen = searchsortedfirst(cumsum(dc.propensity), rand(rng, Uniform(0, total)))
         @assert chosen < length(dc.propensity) + 1
-        key_name = [x for (x, y) in pairs(dc.key) if Base.:(==)(y, chosen)][1]
+        key_name = [x for (x, y) in pairs(dc.key) if y == chosen][1]
         return (when - log(rand(rng)) / total, key_name)
     else
         return (Inf, nothing)
