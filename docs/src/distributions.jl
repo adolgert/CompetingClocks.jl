06b294a85e764816099f9e36a632dd140533ae03
slwu89 <10673535+slwu89@users.noreply.github.com>
2024-03-28 09:00:32 -0700

    add CDF/CCDF plot, hide code in distributions.jl

--- docs/src/distributions.jl
+++ docs/src/distributions.jl
@@ -1,11 +1,11 @@
 # # How Fleck Decides What Happens Next
 
-using DisplayAs
-using Plots
-using Distributions
-using LaTeXStrings
-using QuadGK
-using ForwardDiff
+using DisplayAs #hide
+using Plots #hide
+using Distributions #hide
+using LaTeXStrings #hide
+using QuadGK #hide
+using ForwardDiff #hide
 
 # Fleck simulates generalized semi-Markov processes (GSMP). Every event in a generalized semi-Markov process is chosen as the result of a competion among clocks to see which fires next.
 # 
@@ -15,31 +15,31 @@ using ForwardDiff
 # 
 # Let's say you have a cold. You know you aren't going to recover immediately, but, as days go by, you're more and more sure you'll recover soon. You can read this graph below as the probability to recover given that you have not yet recovered.
 
-survival = ccdf
-hazard(dist, x) = pdf(dist, x) / survival(dist, x)
-conditional_survival(dist, intermediate, x) = survival(dist, x) / survival(dist, intermediate)
-shiftpdf(dist, intermediate, x) = hazard(dist, x) * conditional_survival(dist, intermediate, x)
-
-x = 0:0.01:15
-y = hazard.(Gamma(5.0, 1.0), x)
-p = plot(x, y, label="Gamma(5,1)")
-xlabel!(p, "Time")
-ylabel!(p, "Hazard Rate [per unit time]")
-title!(p, "Hazard of a Gamma Distribution")
-DisplayAs.PNG(p)
+const Gamma51 = Gamma(5.0, 1.0) #hide
+survival = ccdf #hide
+hazard(dist, x) = pdf(dist, x) / survival(dist, x) #hide
+conditional_survival(dist, intermediate, x) = survival(dist, x) / survival(dist, intermediate) #hide
+shiftpdf(dist, intermediate, x) = hazard(dist, x) * conditional_survival(dist, intermediate, x) #hide
+x = 0:0.01:15 #hide
+y = hazard.(Gamma51, x) #hide
+p = plot(x, y, label="Gamma(5,1)") #hide
+xlabel!(p, "Time") #hide
+ylabel!(p, "Hazard Rate [per unit time]") #hide
+title!(p, "Hazard of a Gamma Distribution") #hide
+DisplayAs.PNG(p) #hide
 
 # 
 # It starts at zero, meaning there's no way you'll recover as soon as you're sick. It gets more likely over time that you're at the tail end of being sick. This is called a *hazard rate,* and the hazard rate shown is that of a Gamma distribution, commonly used to describe the rate of recovery for a population of individuals who are sick.
 # 
 # If, instead, you want to see the number of people who recover on any given day, that is called a probability distribution function.
 
-x = 0:0.01:15
-y = pdf.(Gamma(5.0, 1.0), x)
-p = plot(x, y, label="Gamma(5,1)")
-xlabel!(p, "Time")
-ylabel!(p, "Probability Density Function")
-title!(p, "PDF of a Gamma Distribution")
-DisplayAs.PNG(p)
+x = 0:0.01:15 #hide
+y = pdf.(Gamma51, x) #hide
+p = plot(x, y, label="Gamma(5,1)") #hide
+xlabel!(p, "Time") #hide
+ylabel!(p, "Probability Density Function") #hide
+title!(p, "PDF of a Gamma Distribution") #hide
+DisplayAs.PNG(p) #hide
 
 # 
 # Where the hazard rate is an instantaneous quantity at a point in time, the probability distribution function (pdf) integrates over all possible future times. If we call the hazard rate $\lambda(t)$ and call the pdf $f$, we get this relationship.
@@ -52,19 +52,18 @@ DisplayAs.PNG(p)
 # 
 # A simulation, however, has multiple events possible at any one time. One event may happen, and then other events need to restart. Let's ask, if you still have a cold on day 5, what is the probability distribution function for when you will recover?
 
-t=5.0
-
-x1 = 5:0.01:15
-y1 = shiftpdf.(Gamma(5.0, 1.0), t, x1)
-sgp = plot(x1, y1, label="Shifted Gamma(5,1)")
-x = 0:0.01:15
-y = pdf.(Gamma(5.0, 1.0), x)
-plot!(sgp, x, y, label="Gamma(5,1)")
-vline!(sgp, [t], label="New start time")
-xlabel!(sgp, "Time")
-ylabel!(sgp, "Probability Density Function")
-title!(sgp, "Change to PDF After Not Firing")
-DisplayAs.PNG(sgp)
+t=5.0 #hide
+x1 = 5:0.01:15 #hide
+y1 = shiftpdf.(Gamma51, t, x1) #hide
+sgp = plot(x1, y1, label="Shifted Gamma(5,1)") #hide
+x = 0:0.01:15 #hide
+y = pdf.(Gamma51, x) #hide
+plot!(sgp, x, y, label="Gamma(5,1)") #hide
+vline!(sgp, [t], label="New start time") #hide
+xlabel!(sgp, "Time") #hide
+ylabel!(sgp, "Probability Density Function") #hide
+title!(sgp, "Change to PDF After Not Firing") #hide
+DisplayAs.PNG(sgp) #hide
 
 # 
 # The probability distribution function changes now that you know you didn't recover earlier than day 5. On the other hand, the hazard rate for recovery from the cold will be unchanged. Using the same hazard rate, we can recalculate the pdf from the new time $t_0=5$.
@@ -86,36 +85,45 @@ DisplayAs.PNG(sgp)
 # ```math
 # S(t_1) = 1 - F(t_1;t_1) = e^{-\int_{t_0}^{t_1} \lambda(s) ds}
 # ```
-# 
+
+y1 = cdf.(Gamma51, x) #hide
+y2 = survival.(Gamma51, x) #hide
+p = plot(x, y1, label="CDF") #hide
+plot!(p, x, y2, label="Survival") #hide
+xlabel!(p, "Time") #hide
+ylabel!(p, "Probability") #hide
+title!(p, "Cumulative distribution and Survival functions \nof a Gamma Distribution") #hide
+DisplayAs.PNG(p) #hide
+
 # ## Competition
 # 
 # ### Individual Distributions
 # 
 # Let's think of a moment when there are three possible next events. There is a Gamma distribution for when you recover from a cold, a Weibull distribution for when you decide to take medicine for the cold, and an Exponential distribution for when your Mom calls you. Each one is described by a distribution in time, and we can think of them as three hazard rates.
 # 
-com_dists = [
-    Exponential(10),
-    Weibull(1.8, 8),
-    Gamma(5.0, 1.5)
-]
-com_diff_dists = [
-    Exponential(10.0),
-    Weibull(1.8, 8),
-    Gamma{ForwardDiff.Dual}(5.0, 1.5)
-]
-com_labels = ["Exponential(10)" "Weibull(1.8, 8)" "Gamma(5,1.5)"]
-com_x = 0:0.01:15
-com_hazard = stack([hazard.(d, com_x) for d in com_dists])
-a = plot(com_x, com_hazard,
-    label=com_labels,
-    title="Hazards of Individual Distributions")
-com_pdf = stack([pdf.(d, com_x) for d in com_dists])
-b = plot(com_x, com_pdf,
-    label=["Exponential(10)" "Weibull(1.8, 8)" "Gamma(5,1.5)"],
-    title="PDFs of Individual Distributions"
-    )
-indp = plot(a, b, layout=(1, 2), size=(1000, 400))
-DisplayAs.PNG(indp)
+com_dists = [ #hide
+    Exponential(10), #hide
+    Weibull(1.8, 8), #hide
+    Gamma(5.0, 1.5) #hide
+] #hide
+com_diff_dists = [ #hide
+    Exponential(10.0), #hide
+    Weibull(1.8, 8), #hide
+    Gamma{ForwardDiff.Dual}(5.0, 1.5) #hide
+] #hide
+com_labels = ["Exponential(10)" "Weibull(1.8, 8)" "Gamma(5,1.5)"] #hide
+com_x = 0:0.01:15 #hide
+com_hazard = stack([hazard.(d, com_x) for d in com_dists]) #hide
+a = plot(com_x, com_hazard, #hide
+    label=com_labels, #hide
+    title="Hazards of Individual Distributions") #hide
+com_pdf = stack([pdf.(d, com_x) for d in com_dists]) #hide
+b = plot(com_x, com_pdf, #hide
+    label=["Exponential(10)" "Weibull(1.8, 8)" "Gamma(5,1.5)"], #hide
+    title="PDFs of Individual Distributions" #hide
+    ) #hide
+indp = plot(a, b, layout=(1, 2), size=(1000, 400)) #hide
+DisplayAs.PNG(indp) #hide
 
 # 
 # The separate hazard rates are what we put into the simulation. Given their competition, the hazard rates will remain unchanged, but the pdfs will change.
@@ -130,29 +138,29 @@ DisplayAs.PNG(indp)
 # 
 # That gives the graph on the left.
 # 
-function marginal_probability_event(dists, which, t)
-    core_matrix = pdf(dists[which], t)
-    for i in eachindex(dists)
-        if i != which
-            core_matrix *= survival(dists[i], t)
-        end
-    end
-    core_matrix
-end
+function marginal_probability_event(dists, which, t) #hide
+    core_matrix = pdf(dists[which], t) #hide
+    for i in eachindex(dists) #hide
+        if i != which #hide
+            core_matrix *= survival(dists[i], t) #hide
+        end #hide
+    end #hide
+    core_matrix #hide
+end #hide
 
-total_probability(which, lim) = quadgk(t -> marginal_probability_event(com_dists, which, t), 0, lim, rtol=1e-3)[1]
-tp = total_probability.([1, 2, 3], Inf)
-@show tp
-bb = bar(reshape(com_labels, 3), tp, legend=:none, ylabel="Probability of Each Clock")
-com_pdfs = zeros(Float64, length(com_x), length(com_dists))
-for pdfidx in eachindex(com_dists)
-    for (ix, x) in enumerate(com_x)
-        com_pdfs[ix, pdfidx] = marginal_probability_event(com_dists, pdfidx, x)
-    end
-end
-bp = plot(com_x, com_pdfs, labels=com_labels, title="PDFs of Competing Clocks")
-condp = plot(bb, bp, size=(1000, 400))
-DisplayAs.PNG(condp)
+total_probability(which, lim) = quadgk(t -> marginal_probability_event(com_dists, which, t), 0, lim, rtol=1e-3)[1] #hide
+tp = total_probability.([1, 2, 3], Inf) #hide
+@show tp #hide
+bb = bar(reshape(com_labels, 3), tp, legend=:none, ylabel="Probability of Each Clock") #hide
+com_pdfs = zeros(Float64, length(com_x), length(com_dists)) #hide
+for pdfidx in eachindex(com_dists) #hide
+    for (ix, x) in enumerate(com_x) #hide 
+        com_pdfs[ix, pdfidx] = marginal_probability_event(com_dists, pdfidx, x) #hide
+    end #hide
+end #hide
+bp = plot(com_x, com_pdfs, labels=com_labels, title="PDFs of Competing Clocks") #hide
+condp = plot(bb, bp, size=(1000, 400)) #hide
+DisplayAs.PNG(condp) #hide
 # 
 # The graph on the right shows the conditional distribution in time for each event, given that it was the one that fired, so it is $P[t_i | E_i]$. You can see that these distributions don't match the distributions for the individual events. They are modified by competition.
 # 
@@ -167,26 +175,26 @@ DisplayAs.PNG(condp)
 # 
 # If all distributions are Exponential, then they all have a constant hazard rate, and that conditional probability stays the same over time. If any distributions are non-Exponential, then that conditional probability changes.
 # 
-function total_pdf(t)
-    factor1 = zero(Float64)
-    for dist in com_dists
-        factor1 += hazard(dist, t)
-    end
-    factor2 = one(Float64)
-    for dist in com_dists
-        factor2 *= survival(dist, t)
-    end
-    return factor1 * factor2
-end
-a = plot(com_x, total_pdf.(com_x), legend=:none, title="PDF for First to Fire")
+function total_pdf(t) #hide
+    factor1 = zero(Float64) #hide
+    for dist in com_dists #hide
+        factor1 += hazard(dist, t) #hide
+    end #hide
+    factor2 = one(Float64) #hide
+    for dist in com_dists #hide
+        factor2 *= survival(dist, t) #hide
+    end #hide
+    return factor1 * factor2 #hide
+end #hide
+a = plot(com_x, total_pdf.(com_x), legend=:none, title="PDF for First to Fire") #hide
 
-function com_cond_prob(i, t)
-    hazard(com_dists[i], t) / sum(hazard.(com_dists, t))
-end
-com_rel_prob = stack([com_cond_prob.(i, com_x) for i in eachindex(com_dists)])
-b = plot(com_x, com_rel_prob, labels=com_labels, ylabel="Conditional Probability",
-    title="Which Event Fires Given Firing Time")
-DisplayAs.PNG(plot(a, b, size=(1000, 400)))
+function com_cond_prob(i, t) #hide
+    hazard(com_dists[i], t) / sum(hazard.(com_dists, t)) #hide
+end #hide
+com_rel_prob = stack([com_cond_prob.(i, com_x) for i in eachindex(com_dists)]) #hide
+b = plot(com_x, com_rel_prob, labels=com_labels, ylabel="Conditional Probability", #hide
+    title="Which Event Fires Given Firing Time") #hide
+DisplayAs.PNG(plot(a, b, size=(1000, 400))) #hide
 
 # 
 # On the left of this graph is the pdf for the first event of the three to fire. We can see this as a marginal $P[t]$ and then the right-hand graph as the conditional $P[E_i|t]$.
