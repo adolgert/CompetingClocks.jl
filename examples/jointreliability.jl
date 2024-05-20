e973e41889c6c8d946f01b00609d34ca2c3072c5
Drew Dolgert <adolgert@uw.edu>
2024-05-20 08:18:49 -0400

    working on the text adding headers and connections

--- examples/jointreliability.jl
+++ examples/jointreliability.jl
@@ -1,59 +1,79 @@
-# # Reliability of a Group
+# # Reliability Model of a Work Crew
+
+# This is an extended example using a reliability model.
+
 using Distributions
-using Random
 using Fleck
 using Logging
-using StatsPlots
+using Random
 
+# ## Overview
 #
-# Generalized semi-Markov processes are often used for reliability modeling.
-# A simple reliability model for some vehicle might say it is either
-# working or in repair. There is a distribution of the working time and
-# a distribution of the repair time. Some models make repairs take A
-# fixed amount of time. The Fleck library can certainly model an individual,
-# but let's consider a group of vehicles.
+# The classic model for reliability is of a light bulb that is either
+# lit or broken. There is a distribution of failure times and a distribution
+# of repair times [1]. Let's extend this idea to the reliability of a vehicle
+# motor pool.
 #
-# We need to use ten vehicles a day, and we start with eighteen vehicles.
-# As vehicles go into repair, the ten for the day are chosen from the
-# remaining usable vehicles.
+#  1. There are 16 vehicles.
+#  2. Every morning, 10 vehicles go out for work. They all leave in the first 15 mins.
+#  3. Each vehicle works at least half a day, at most the whole day.
+#  4. While working, a vehicle can break, where the chance of breaking depends
+#     on the total time since it was last repaired.
+#  5. There is a distribution of repair times.
 #
-# If we think about one individual, the states are clear.
-@enum IndividualState indnotset ready working broken
+# Number two, above, says that vehicles start in the first 15 minutes. This
+# plan will initiate ten transitions in rapid succession. We could,
+# instead, start all ten vehicles at the same time, using a single transition.
+# Either would work.
+#
+# ![Timeline of Working and Repair](timeline_month.png)
+#
+# Fleck will take care of the timing of all of the events, but we will see that
+# there is plenty of work to track the state of all of the vehicles. This
+# extended example shows that, if we wanted to create more reliability models,
+# it would make sense to create a framework for reliability modeling, one that
+# uses Fleck.
+#
+# ## Define State for the Model
+#
+# If we think about an individual vehicle, the states are ready, working, or broken.
+#
+@enum IndividualState ready working broken
 
-# For an individual, there are also some clear transitions.
+# There are four allowed tansitions among the three states because a vehicle
+# breaks only while it's working.
+#
 const IndividualTransitions = Dict(
     :work => (ready, working),
     :done => (working, ready),
     :break => (working, broken),
     :repair => (broken, ready)
 )
-
+#
+# An individual has state and parameters. In the language of generalized
+# semi-Markov processes, this state is called the "physical state."
+#
 mutable struct Individual
+    ## State for the individual
     state::IndividualState
-    transition_start::Float64
-    ## This is how an individual remembers its total work leading to failure.
-    work_age::Float64
-    work_dist::LogUniform
+    work_age::Float64 ## How an individual remembers its total work leading to breaks.
+    transition_start::Float64  ## This is bookkeeping.
+    ## Parameters for the individual
+    done_dist::LogUniform
     fail_dist::LogNormal
     repair_dist::Weibull
     Individual(work, fail, repair) = new(
         ready, 0.0, 0.0, work, fail, repair
         )
 end
-
-
-# It's more complicated to consider how we start multiple individuals working
-# on any given day. We have to think about the change in state for the whole
-# simulation. What are all combinations of starting and final states, and
-# how do we ensure they reflect our use case of a motor pool?
 #
-# There can be unexpected corner cases, such as when an individual finishes
-# work during the 6-6:15am when jobs are being scheduled. We simplify this
-# by handling the two individual transitions that change behavior for the
-# whole group. These are the moment that a worker fills up the work slots
-# for the day and the moment a worker frees up a work slot, either by
-# being done or by breaking.
-
+# The simulation as a whole is the state of the individuals and the system time.
+# We put some parameters here:
+#
+#  * `workers_max` - Each morning, this many vehicles start driving, if at least
+#    this many vehicles are ready, instead of broken.
+#  * `start_time` - Vehicles start in the first 15 min or so, and this is that 15 min.
+#
 mutable struct Experiment
     time::Float64
     group::Vector{Individual}
@@ -63,34 +83,62 @@ mutable struct Experiment
     rng::Xoshiro
     Experiment(group::Vector, crew_size::Int, rng) = new(0.0, group, crew_size, 0.01, rng)
 end
-
-
+#
+# Make a simulation by making individuals.
+#
 function Experiment(individual_cnt::Int, crew_size::Int, rng)
-    work_rate = LogUniform(.8, 0.99) # Gamma(9.0, 0.2)
+    done_rate = LogUniform(.8, 0.99) # Gamma(9.0, 0.2)
     break_rate = LogNormal(1.5, 0.4)
     repair_rate = Weibull(1.0, 2.0)
-    workers = [Individual(work_rate, break_rate, repair_rate) for _ in 1:individual_cnt]
+    workers = [Individual(done_rate, break_rate, repair_rate) for _ in 1:individual_cnt]
     Experiment(workers, crew_size, rng)
 end
-
-
+#
+# Some helpers. The `key_type` says that we will track transitions using
+# a tuple of (index of vehicle, symbol to identify the transition).
+#
 key_type(::Experiment) = Tuple{Int,Symbol}
 worker_cnt(experiment::Experiment) = size(experiment.group, 1)
-
-
-# If every ready worker has an active `:work` transition, then there
-# must be a well-defined time for that transition, even if the
-# woker becomes ready between 6:00 am and 6:15 am.
-function next_work_time(now, max_hour)
+#
+# ## Define Transitions for the Model
+#
+# If we were modeling one individual, transitions would be very simple,
+# but by asking that ten vehicles work every morning, we require that those
+# individuals interact.
+#
+# One way to think clearly about interactions is to think about the state
+# of the whole system. If less than ten vehicles are currently working, then
+# every ready vehicle must have an enabled transition to start work at the
+# next available time. Once the tenth vehicle begins working, all of those
+# transitions need to be disabled.
+#
+# It's implied that the start of the day happens at 1.0, 2.0, 3.0. When a
+# vehicle becomes ready, or when the total working vehicles drops below
+# ten, then each ready vehicle could work at a future time. This function
+# takes in the current time and returns two times, relative to the current
+# time, between which the vehicle can start work.
+#
+function next_work_time(now, fifteen_minutes)
     hour = now - floor(now)
-    if hour < max_hour
-        return 0.0, max_hour - hour
-    else
-        return one(hour) - hour, one(hour) + max_hour - hour
+    if hour < fifteen_minutes ## If vehicles are still going out today.
+        return 0.0, fifteen_minutes - hour
+    else ## You can't start until tomorrow.
+        return one(hour) - hour, one(hour) + fifteen_minutes - hour
     end
 end
-
-
+#
+# Now we handle simulation events. This function's complexity is an argument
+# for using a framework like a queueing model, a generalized stochastic Petri
+# net, or some other continuous-time simulation framework.
+#
+# The arguments are:
+# 
+#  * `when` - The time of the next event.
+#  * `(who, transition)` - This exands the `key_type`, which identifies the transition.
+#  * `experiment` - It's our simulation data.
+#  * `sampler` - This is a `Sampler` from Fleck to enable and disable transitions.
+#
+# The first few statements of the function are automatic for any transition.
 function handle_event(when, (who, transition), experiment, sampler)
     start_state, finish_state = IndividualTransitions[transition]
     individual = experiment.group[who]
@@ -99,17 +147,26 @@ function handle_event(when, (who, transition), experiment, sampler)
     experiment.time = when
     disable!(sampler, (who, transition), when)
 
-    ## First look at what happens to an individual.
+# If a vehicle is done work, or if they break, then include the time worked
+# in their total work age.
     if start_state == working
         work_duration = when - individual.transition_start
         @debug "Adding $work_duration to $who"
         individual.work_age += work_duration
     end
-
+#
+# The state of the system, as a whole, depends on the total number
+# currently working.
+#
     worker_cnt = count(w.state == working for w in experiment.group)
     need_workers = worker_cnt < experiment.workers_max
     max_hour = experiment.start_time
-
+#
+# When an individual was working, there were two possible transitions,
+# one to `ready`, and one to `broken`. Don't forget to disable the `:break`
+# transition. Then schedule the next day's work only if the system has less
+# than ten working.
+#
     if transition == :done
         disable!(sampler, (who, :break), when)
         if need_workers
@@ -117,23 +174,33 @@ function handle_event(when, (who, transition), experiment, sampler)
             enable!(sampler, (who, :work), rate, when, when, experiment.rng)
             @debug "schedule $who for $rate"
         end
-
+#
+# A `:repair` transition can happen at any time, including during the first
+# fifteen minutes of a day.
+#
     elseif transition == :repair
         if need_workers
             rate = Uniform(next_work_time(when, max_hour)...)
             enable!(sampler, (who, :work), rate, when, when, experiment.rng)
             @debug "schedule $who for $rate"
         end
-    
+#
+# The `:work` transition represents a vehicle going out to work for the day.
+# This enables two possible transitions, finishing work or breaking. The
+# breaking transition is interesting because it has what Zimmerman [2] calls
+# "memory." It remembers how long it was previously enabled
+#    
     elseif transition == :work
         # enable :done and :break
-        enable!(sampler, (who, :done), individual.work_dist, when, when, experiment.rng)
+        enable!(sampler, (who, :done), individual.done_dist, when, when, experiment.rng)
         ## Time shift this distribution to the left because it remembers
         ## the time already worked.
         past_work = when - individual.work_age
         enable!(sampler, (who, :break), individual.fail_dist, past_work, when, experiment.rng)
         @debug "schedule $who for done or break"
-
+#
+# When a vehicle breaks, the only option is to repair it. This resets the work age.
+#
     elseif transition == :break
         # If you broke, you don't get to finish your work.
         disable!(sampler, (who, :done), when)
@@ -145,8 +212,14 @@ function handle_event(when, (who, transition), experiment, sampler)
         @assert finish_state ∈ (broken, working, ready)
     end
     individual.transition_start = when
-
-    ## Then look at changes to behavior of the whole system.
+#
+# We haven't handled how we ensure that at most ten vehicles start work every
+# morning. For that, we need to think about the system as a whole, explicitly
+# by looking at the current worker count and whether it crossed the threshold of
+# ten workers.
+#
+# If a vehicle just started and is the tenth worker, then cancel the ability of
+# all other vehicles to work.
     if transition == :work && worker_cnt == experiment.workers_max
         notnow = Int[]
         for too_many in [widx for (widx, w) in enumerate(experiment.group) if w.state == ready]
@@ -155,6 +228,11 @@ function handle_event(when, (who, transition), experiment, sampler)
             push!(notnow, too_many)
         end
         @debug "Unscheduling $notnow"
+#
+# If a vehicle stopped work, either by finishing or breaking, and it was the
+# first of the work crew to quit, then notify all `ready` vehicles that they
+# should start work at the start of the next morning.
+#
     elseif transition ∈ (:done, :break) && worker_cnt == experiment.workers_max - 1
         rate = Uniform(next_work_time(when, max_hour)...)
         upnext = Int[]
@@ -167,8 +245,110 @@ function handle_event(when, (who, transition), experiment, sampler)
         @debug "scheduling $upnext for $rate"
     end
 end
+#
+# ## Configure the Model
+#
+# For anything other than an example, the most important step would be
+# configuring the model so that it matches observations. Here, however, we
+# have put this directly into the `Experiment` type.
+#
+function show_distributions()
+    experiment = Experiment(16, 10, Xoshiro(9378424))
 
+    plot(experiment.group[1].done_dist, label="Done")
+    plot!(experiment.group[1].fail_dist, label="Break")
+    plot!(experiment.group[1].repair_dist, label="Repair")
+    title!("Distributions for Transitions")
+end
+show_distributions()
 
+# ## Run the Simulation
+#
+# Running a simulation means we are sampling from the stochastic process.
+# For a continuous-time stochastic process like this, that means asking the
+# sampler when the next transition is and which transition it is. If there
+# are no possible transitions, the next time will be infinite.
+#
+function run(experiment::Experiment, observation, days)
+    sampler = FirstToFire{key_type(experiment),Float64}()
+    rng = experiment.rng
+    rate = Uniform(next_work_time(0.0, experiment.start_time)...)
+    for initial in 1:length(experiment.group)
+        enable!(sampler, (initial, :work), rate, 0.0, 0.0, rng)
+    end
+    when, which = next(sampler, experiment.time, rng)
+    while isfinite(when) && when < days
+        ## We use different observers to record the simulation.
+        observe(experiment, observation, when, which)
+        @debug "$when $which"
+        handle_event(when, which, experiment, sampler)
+        when, which = next(sampler, experiment.time, rng)
+    end
+end
+#
+# ## Observers
+#
+# While writing a simulation that matches a real world model requires precision,
+# observations of simulation runs are comparatively messy. Continuous-time
+# models have a separate transition and time for every change to the state,
+# so they can generate a lot of data quickly. It doesn't make sense to save
+# the raw data stream, so we use observers of the system to summarize that
+# data, almost as a first step of analysis. Let's look at a few examples
+# for the vehicle crew.
+#
+# ### Continuous-time Summary Observer
+#
+# The first example will record data for every transition, but it will record
+# only the total number of working or broken vehicles. This is what gave us the
+# plot at the top of this page.
+#
+# This represents a single time point.
+struct ContinuousRec
+    working::Int64
+    broken::Int64
+    total_age::Float64
+    time::Float64
+end
+
+# We will have a vector of those single time points.
+mutable struct ObserveContinuous
+    state::Vector{ContinuousRec}
+    ObserveContinuous() = new([ContinuousRec(0, 0, 0.0, 0.0)])
+end
+#
+# This observer keeps a running sum of the number working and broken.
+# Note that it has to know how different transitions change those numbers.
+# The relationship between the transition and how it changes counts is called
+# stoichiometry, because it was first observed for chemical reactions.
+# Both chemical simualtions and GSPN would have this information encoded in
+# a formal model.
+#
+function observe(experiment::Experiment, observation::ObserveContinuous, when, which)
+    who, transition = which
+    working = observation.state[end].working
+    broken = observation.state[end].broken
+    if transition == :work
+        working += 1
+    elseif transition == :done
+        working -= 1
+    elseif transition == :break
+        broken += 1
+        working -= 1
+    elseif transition == :repair
+        broken -= 1
+    else
+        @assert transition ∈ (:work, :done, :break, :repair)
+    end
+    total_age = sum(w.work_age for w in experiment.group)
+    push!(observation.state, ContinuousRec(working, broken, total_age, when))
+end
+#
+# ### Once-a-day Observation of the Working State
+#
+# Suppose that we want to match our simulation to observation data that counts,
+# every day, how many vehicles went out and how many were broken that morning.
+# This observer records that status each day.
+#
 mutable struct ObserveLots
     status::Array{Int64,2}
     started_today::Array{Int64,1}
@@ -183,8 +363,10 @@ mutable struct ObserveLots
 end
 
 days(observation::ObserveLots) = size(observation.status, 2)
-
-
+#
+# This observer waits until the current transition time is just after the
+# first 15min of the day. Then it records every vehicle's status.
+#
 function observe(experiment::Experiment, observation::ObserveLots, when, which)
     who, transition = which
     day_idx = Int(floor(when))
@@ -206,8 +388,14 @@ function observe(experiment::Experiment, observation::ObserveLots, when, which)
         end
     end
 end
-
-
+#
+# ### Distribution of Broken Vehicles and Probability of Missing Crew
+#
+# If we were focused more on the small probability that there wouldn't be
+# enough vehicles in the morning to start a full ten, then we want to
+# understand the histogram of how many vehicles are broken on any given
+# day.
+#
 mutable struct ObserveHistogram
     counts::Array{Int64,2}
     working::Int64
@@ -216,9 +404,18 @@ mutable struct ObserveHistogram
     ObserveHistogram(e::Experiment, burn) = new(
         zeros(Int64, e.workers_max + 1, worker_cnt(e) + 1), 0, 0, burn)
 end
-
+must_work(o::ObserveHistogram) = size(o.counts, 1)
+total_workers(o::ObserveHistogram) = size(o.counts, 2)
 
 function observe(experiment::Experiment, observation::ObserveHistogram, when, which)
+    if when > observation.burn
+        day_start = Int(floor(experiment.time + next_work_time(experiment.time, experiment.start_time)[1]))
+        next_start = Int(floor(when + next_work_time(when, experiment.start_time)[1]))
+        if day_start != next_start
+            observation.counts[observation.working + 1, observation.broken + 1] += 1
+        end
+    end
+
     who, transition = which
     if transition == :work
         observation.working += 1
@@ -232,34 +429,12 @@ function observe(experiment::Experiment, observation::ObserveHistogram, when, wh
     else
         @assert transition ∈ (:work, :done, :break, :repair)
     end
-
-    if when < observation.burn
-        return
-    end
-
-    day_start = Int(floor(experiment.time + next_work_time(experiment.time, experiment.start_time)[1]))
-    next_start = Int(floor(when + next_work_time(when, experiment.start_time)[1]))
-    if day_start != next_start
-        observation.counts[observation.working + 1, observation.broken + 1] += 1
-    end
 end
 
-
-function run(experiment::Experiment, observation, days)
-    day_cnt = Int(ceil(days))
-    sampler = FirstToFire{key_type(experiment),Float64}()
-    rng = experiment.rng
-    rate = Uniform(next_work_time(0.0, experiment.start_time)...)
-    for initial in 1:length(experiment.group)
-        enable!(sampler, (initial, :work), rate, 0.0, 0.0, rng)
-    end
-    when, which = next(sampler, experiment.time, rng)
-    while isfinite(when) && when < days
-        observe(experiment, observation, when, which)
-        @debug "$when $which"
-        handle_event(when, which, experiment, sampler)
-        when, which = next(sampler, experiment.time, rng)
-    end
-end
-
-# Let's make an experiment and look at the distributions.
+# ## References
+#
+# 1. Limnios, Nikolaos, and Gheorghe Oprisan. Semi-Markov processes and
+#    reliability. Springer Science & Business Media, 2012.
+#
+# 2. Zimmermann, Armin. Stochastic discrete event systems. Springer, Berlin
+#    Heidelberg New York, 2007.
--- examples/test_jointreliability.jl
+++ examples/test_jointreliability.jl
@@ -1,6 +1,8 @@
 include("jointreliability.jl")
 
 using Logging
+using ColorSchemes
+using StatsPlots
 
 for hour in [0.005, 0.7]
     for day in [0.0, 1.0, -5.0, 17.0]
@@ -18,20 +20,6 @@ break_rate = LogNormal(3.3, 0.4)
 repair_rate = Weibull(1.0, 5.0)
 joe = Individual(work_rate, break_rate, repair_rate)
 
-rng = Xoshiro(9378424)
-experiment = Experiment(16, 10, rng)
-
-plot(experiment.group[1].work_dist)
-title!("Work Duration Distribution")
-png("work.png")
-plot(experiment.group[1].fail_dist)
-title!("Distribution of Failures")
-png("fail.png")
-plot(experiment.group[1].repair_dist)
-title!("Repair Time Durations")
-png("fix.png")
-
-
 struct OnlyMyLogger <: AbstractLogger
     instance::ConsoleLogger
     OnlyMyLogger() = new(ConsoleLogger(stdout, Logging.Debug))
@@ -43,21 +31,122 @@ end
 Logging.min_enabled_level(logger::OnlyMyLogger) = Logging.min_enabled_level(logger.instance)
 Logging.handle_message(logger::OnlyMyLogger, args...; kwargs...) = Logging.handle_message(logger.instance, args...; kwargs...)
 
-debug_logger = OnlyMyLogger()
-day_cnt = 20000
-observation = ObserveHistogram(experiment, day_cnt ÷ 10)
-run(experiment, observation, day_cnt)
-println(observation.counts)
-heatmap(observation.counts)
-xlabel!("Broken")
-ylabel!("Worked")
-png("counts.png")
-# with_logger(debug_logger) do
-#     day_cnt = 2000
-#     observation = ObserveLots(day_cnt, worker_cnt(experiment))
-#     run(experiment, observation, day_cnt)
-#     plot(1:day_cnt, observation.status[2, :], seriestype=:scatter, label="repair")
-#     plot!(1:day_cnt, observation.status[1, :], seriestype=:scatter, label="working")
-#     png("record.png")
-#     println(100 * observation.broken_duration / day_cnt)
-# end
+function plotgrid(observation::ObserveHistogram)
+    heatmap(observation.counts, c = cgrad(:acton))
+    xlabel!("Broken")
+    ylabel!("Worked")
+    png("counts.png")
+end    
+
+function compare_across_workers(obs::Vector{ObserveHistogram}, labels, title)
+    firstplot = true
+    for (obs_idx, observation) in enumerate(obs)
+        worker_cnt = total_workers(observation)
+        broken = vec(sum(observation.counts, dims=1))
+        cnt = findlast(broken .> 0)
+        normed = broken[1:cnt] / sum(broken)
+        if firstplot
+            firstplot = false
+            plot(1:cnt, normed, seriestype=:scatter, label=labels[obs_idx], markersize=8)
+        else
+            plot!(1:cnt, normed, seriestype=:scatter, label=labels[obs_idx], markersize=8)
+        end
+    end
+    xlabel!("Count of Broken")
+    ylabel!("Probability Mass Function")
+    title!(title)
+    png(replace(title, " "=>"") * ".png")
+end
+
+
+function plot_timeline(obs::ObserveContinuous, experiment::Experiment)
+    state = obs.state
+    last_time = state[end].time
+    first_idx = findlast([x.time < last_time - 5 for x in state])
+    times = [x.time for x in state[first_idx:end]]
+    times .-= 4009
+    working = [x.working for x in state[first_idx:end]]
+    broken = [x.broken for x in state[first_idx:end]]
+    ready = [worker_cnt(experiment) - x.working - x.broken for x in state[first_idx:end]]
+    plot(times, working, label="working", line=(:steppost, 2))
+    plot!(times, broken, label="broken", line=(:steppost, 2))
+    # plot!(times, ready, label="ready", line=(:steppost, 2))
+    xlabel!("Time [days]")
+    ylabel!("Status [count]")
+    title!("Timeline of Work Crew Over Five Days")
+    png("timeline_month.png")
+end
+
+
+function show_number_broken()
+    rng = Xoshiro(9378424)
+    observations = ObserveHistogram[]
+    labels = String[]
+    years = 100
+    day_cnt = 365 * years
+
+    for worker_cnt in [10, 11, 12, 13, 14, 16, 20, 100]
+        experiment = Experiment(worker_cnt, 10, rng)
+        burn = min(day_cnt ÷ 10, 3650)
+        observation = ObserveHistogram(experiment, burn)
+        run(experiment, observation, day_cnt)
+        push!(observations, observation)
+        push!(labels, string(worker_cnt))
+    end
+
+    compare_across_workers(observations, labels, "Number Broken as Workers Increase")
+end
+
+
+function walk_simulation()
+    debug_logger = OnlyMyLogger()
+    rng = Xoshiro(234987)
+    with_logger(debug_logger) do
+        day_cnt = 20
+        experiment = Experiment(worker_cnt, 10, rng)
+        observation = ObserveLots(day_cnt, worker_cnt(experiment))
+        run(experiment, observation, day_cnt)
+        plot(1:day_cnt, observation.status[2, :], seriestype=:scatter, label="repair")
+        plot!(1:day_cnt, observation.status[1, :], seriestype=:scatter, label="working")
+        png("record.png")
+        println(100 * observation.broken_duration / day_cnt)
+    end
+end
+
+
+function show_competition_effect()
+    rng = Xoshiro(4377124)
+    observations = ObserveHistogram[]
+    labels = String[]
+    years = 1000
+    day_cnt = 365 * years
+    worker_cnt = 20
+    for must_work in [1, 5, 10, 15, 20]
+        experiment = Experiment(worker_cnt, must_work, rng)
+        burn = min(day_cnt ÷ 10, 3650)
+        observation = ObserveHistogram(experiment, burn)
+        run(experiment, observation, day_cnt)
+        push!(observations, observation)
+        push!(labels, string(must_work))
+    end
+
+    compare_across_workers(observations, labels, "Number Broken as Crew Increases")
+end
+
+
+function show_typical_timeline()
+    rng = Xoshiro(9234232)
+    years = 11
+    day_cnt = 365 * years
+    worker_cnt = 16
+    observation = ObserveContinuous
+    experiment = Experiment(worker_cnt, 10, rng)
+    observation = ObserveContinuous()
+    run(experiment, observation, day_cnt)
+
+    plot_timeline(observation, experiment)
+end
+
+# show_competition_effect()
+# show_number_broken()
+# show_typical_timeline()
