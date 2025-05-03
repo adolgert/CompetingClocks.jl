b82b64477cff965ab882dbb4747d0a8462db931c
slwu89 <10673535+slwu89@users.noreply.github.com>
2025-05-03 09:38:38 -0700

    tests for most recent enabling tracker

--- test/test_track.jl
+++ test/test_track.jl
@@ -33,34 +33,29 @@ end
     using Distributions
 
     propagator = MemorySampler(FirstToFire{Int64,Float64}())
-    
     rng = Xoshiro(39472)
 
-    incremental_enable = 0.0
-    firing_times = [(1, 7.9), (2, 12.3), (3, 3.7), (4, 1.3), (5, 0.2)]
-    for (clock, when_fire) in firing_times
-        enable!(propagator, clock, Dirac(when_fire), incremental_enable, incremental_enable, rng)
-        incremental_enable += 0.01
+    clock_key = 1:5
+    relative_firing = [7.9, 12.3, 3.7, 1.3, 0.2]
+    enabling_times = [0.0; cumsum(fill(0.01, 4), dims=1)]
+    for i in eachindex(relative_firing)
+        enable!(propagator, clock_key[i], Dirac(relative_firing[i]), enabling_times[i], enabling_times[i], rng)
     end
-    @test absolute_enabling(propagator, 1) == 0.0
-    
-    @test absolute_enabling(propagator, 3) ≈ 0.02
-    @test absolute_enabling(propagator, 5) ≈ 0.04
-    
-    sort!(firing_times, by=x -> x[2])
-    last_fired = 0.0
-    for (key, fire_time) in firing_times
-        (t1, k1) = next(propagator, last_fired, rng)
-        @test k1 == key
-        last_fired = t1
-        if k1 !== nothing
-            disable!(propagator, k1, last_fired)
-        end
-    end
-    # (t2, k2) = next(propagator, 0.0, rng)
-    # @test k2 === nothing
 
-    # reset!(propagator)
+    # test retrieving the enabling times
+    for i in eachindex(relative_firing)
+        @test absolute_enabling(propagator, clock_key[i]) == enabling_times[i]
+    end
+
+    absolute_firing = enabling_times + relative_firing
+    clock_firing_order = sortperm(absolute_firing)
+    last_fired = 0.0 
+    for i in eachindex(clock_firing_order)
+        (last_fired, which) = next(propagator, last_fired, rng)
+        @test last_fired ≈ absolute_firing[clock_firing_order[i]]
+        disable!(propagator, which, last_fired)
+        @test_throws KeyError absolute_enabling(propagator, clock_key[clock_firing_order[i]])
+    end
 end
 
 @safetestset track_trajectory_comparison = "TrajectoryWatcher comparison" begin
